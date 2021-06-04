176508d27b24412d925f4e192f1cbdeba45a352f
tuanlt79 <tuanlt79@outlook.com.vn>
2021-06-04 09:26:31 +0700

    sosanh_v1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const guessNumber = Math.round(Math.random() * 100);
+const doan = [];
+const doanSo = () => {
+  const guessNumber = Math.round(Math.random() * 2);
+  console.log(guessNumber);
+  const inputNhap = document.getElementById("inputNhap").value;
+  let txtKQ = "";
+  if (guessNumber < inputNhap) {
+    console.log("số nhập vào lớn hơn số cần tìm ");
+    txtKQ = "Số nhập vào lớn hơn số cần tìm";
+
+    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+  } else if (guessNumber > inputNhap) {
+    console.log("số nhập vào nhỏ hơn số cần tìm");
+    txtKQ = "Số nhập vào nhỏ hơn số cần tìm";
+    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+  } else {
+    console.log("chúc mừng bạn đã đoán đúng số");
+    txtKQ = "Chúc mừng bạn đã đoán đúng số";
+    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+  }
+};
+
+document.getElementById("btnDoan").onclick = (e) => {
+  e.preventDefault();
+  doanSo();
+};
