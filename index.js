7a44c2ab938fd46c014061daa3d24d2c9a93d54d
Digilife <tuanlt79@outlook.com.vn>
2021-06-04 10:42:16 +0700

    es6_bt_1

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const doan = [];
+let count = 1;
 const doanSo = () => {
-  const guessNumber = Math.round(Math.random() * 2);
+  const guessNumber = Math.round(Math.random() * 100);
   console.log(guessNumber);
   const inputNhap = document.getElementById("inputNhap").value;
   let txtKQ = "";
-  if (guessNumber < inputNhap) {
-    console.log("số nhập vào lớn hơn số cần tìm ");
-    txtKQ = "Số nhập vào lớn hơn số cần tìm";
+  let txtKQ1 = "Số nhập vào lớn hơn số cần tìm";
+  let txtKQ2 = "Số nhập vào nhỏ hơn số cần tìm";
 
-    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+  if (guessNumber < inputNhap) {
+    txtKQ = txtKQ1;
+    console.log(txtKQ);
   } else if (guessNumber > inputNhap) {
-    console.log("số nhập vào nhỏ hơn số cần tìm");
-    txtKQ = "Số nhập vào nhỏ hơn số cần tìm";
-    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+    txtKQ = txtKQ2;
+    console.log(txtKQ);
   } else {
-    console.log("chúc mừng bạn đã đoán đúng số");
     txtKQ = "Chúc mừng bạn đã đoán đúng số";
-    return (document.getElementById("txtKQ").innerHTML = txtKQ);
+    console.log(txtKQ);
   }
+  document.getElementById("txtKQ").innerHTML = txtKQ;
+  if (count >= 0) {
+    let divTag = document.createElement("div");
+    divTag.id = "historyCount";
+    divTag.innerHTML = `Lần ${count}: ${inputNhap}`;
+    document.body.appendChild(divTag);
+  }
+  count++;
 };
 
 document.getElementById("btnDoan").onclick = (e) => {
